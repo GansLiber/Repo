1dca2752c47b40cd0d7f5a895731e4e76e6188f7
zazaz <zazaza993@mail.ru>
2025-05-05 17:17:29 +0700

    Смена пароля, выход студента, стили

--- tutor_site/scheduler/urls.py
+++ tutor_site/scheduler/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(
+        template_name='scheduler/password_change.html',
+        success_url='/password_change/done/'
+    ), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(
+        template_name='scheduler/password_change_done.html'
+    ), name='password_change_done'),
     path('tutor/', views.tutor_dashboard, name='tutor_dashboard'),
     path('student/', views.student_dashboard, name='student_dashboard'),
     path('tutor/create-slot/', views.create_time_slot, name='create_time_slot'),
